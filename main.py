d60c851a087df2c0f86c9662fbe05f95a2f1fc8e
Ryli Davis <ryli_davis@intuit.com>
2022-10-20 14:58:37 -0700

    number of lines fn-testing

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+# All capitals mean constant value - something that is not going to change.
+MAX_LINES = 3
+
 # collecting user input : We need to get the deposit that the user
 # is entering as well as their bet.
 
@@ -24,4 +27,31 @@ def deposit():
 
     return amount
 
-deposit()
+def get_number_of_lines():
+    while True:
+        lines = input("Enter the number of lines to bet on (1-" + str(MAX_LINES) + ")? ")
+# now we need to check if this amount is actually a valid number. isdigit
+# is going to tell us if this is a valid whole number. Negative numbers
+# won't be true. 
+        if lines.isdigit():
+# By default the number the user enters will come as a string
+# so we will convert it into a number(int)
+            lines =int(lines)
+# if amount is greater than 0 then we will break out of this while loop. 
+            if 1 <= lines <= MAX_LINES:
+                break
+            else: 
+                print("Enter a valid number of lines.")
+        else:
+            print("Please enter a number.")
+
+    return lines
+
+#I'm going to put our program in this function main so that if we end our
+# program, we can just call this function again and it will rerun program.
+def main():
+    balance = deposit()
+    lines = get_number_of_lines()
+    print(balance, lines)
+
+main()
